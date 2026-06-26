ebab5097fb62351aaea59323cfd14cf0673652d1
VIbhuti Naik <vibhutinaik1407@gmail.com>
2026-06-26 08:33:54 +0530

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -4,7 +4,7 @@ import pickle
 import plotly.express as px
 from analytics import inject_ga
 
-inject_ga(page_title="SentinelAI - Overview Dashboard", page_path="/overview")
+inject_ga(page_title="ChurnSentinel - Overview Dashboard", page_path="/overview")
 
 st.markdown("<style>[data-testid='stSidebarNav'] { display: none !important; }</style>", unsafe_allow_html=True)
 
